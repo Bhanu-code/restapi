2265339bac65bfac7f8112de3d47639eb95aa12b
unknown <Bhanu-code>
2023-04-25 16:49:18 +0530

    auth added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,13 @@
 
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 const getGoals = asyncHandler( async (req, res)=>{
-    res.status(200).json({message: 'Get goals'})
+
+    const goals = await Goal.find({user: req.user.id})
+
+    res.status(200).json(goals)
 })
 
 const setGoals = asyncHandler( async (req, res)=>{
@@ -10,16 +15,64 @@ const setGoals = asyncHandler( async (req, res)=>{
         res.status(400)
         throw new Error('please add some text')
     }
-    res.status(200).json({message: 'Set goals'})
+
+    const goals = await Goal.create({
+        text : req.body.text,
+        user: req.user.id,
+    })
+
+    res.status(200).json(goals)
 })
 
 const putGoals = asyncHandler( async (req, res)=>{
+
+    const goals = await Goal.findById(req.params.id)
+
+    if(!goals){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    if(goals.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('user not authorized')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true,})
     
-    res.status(200).json({message: `Update goals ${req.params.id}` })
+    res.status(200).json(updatedGoal)
 })
 
 const deleteGoals = asyncHandler( async (req, res)=>{
-    res.status(200).json({message: `Delete goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    if(goals.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('user not authorized')
+    }
+    
+    await goal.deleteOne({_id:req.params.id});
+
+    res.status(200).json({_id : req.params.id})
 })
 
 module.exports = {
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT;
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB();
+
 
 const app = express()
 
@@ -9,6 +13,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.use(errorHandler)
 
 app.listen(port, ()=>console.log(`server is running on port ${port}`))
